43fea61db1343ac5d9e9c8fe38dc3dc1fe7c8e11
huxh7500 <86956846+huxh7500@users.noreply.github.com>
2021-10-09 14:40:30 +0300

    local host change

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -26,35 +26,14 @@ const HomeScreen = ({ match }) => {
 
   return (
     <div>
-      <Meta />
-      {!keyword ? (
-        <ProductCarousel />
+      {!products ? (
+        <Loader />
       ) : (
         <Link to='/' className='btn btn-light'>
           Go Back
         </Link>
       )}
-      <h1>Latest Products</h1>
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant='danger'>{error}</Message>
-      ) : (
-        <div>
-          <Row>
-            {products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
-          <Paginate
-            pages={pages}
-            page={page}
-            keyword={keyword ? keyword : ''}
-          />
-        </div>
-      )}
+      
     </div>
   )
 }
