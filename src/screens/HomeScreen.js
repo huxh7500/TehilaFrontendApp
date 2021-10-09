e13972ee7cacfdd1e8a696bfce36a7b5c151b118
huxh7500 <86956846+huxh7500@users.noreply.github.com>
2021-10-09 15:04:40 +0300

    local host change

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -19,7 +19,7 @@ const HomeScreen = ({ match }) => {
   const dispatch = useDispatch()
 
   const productList = useSelector((state) => state.productList)
-  const { loading, error, products, page, pages } = productList
+  const { loading, error, products=[], page, pages } = productList
 
   useEffect(() => {
     dispatch(listProducts(keyword, pageNumber))
